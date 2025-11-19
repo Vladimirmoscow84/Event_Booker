bb1021c2385761f3ff7a68cd5bf3cd6532a7a94d
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-20 00:44:18 +0300

    begin methods postgres

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -1,7 +1,42 @@
 package postgres
 
-import "github.com/jmoiron/sqlx"
+import (
+	"fmt"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type Postgres struct {
 	DB *sqlx.DB
+}
+
+func New(databaseURI string) (*Postgres, error) {
+	db, err := sqlx.Connect("pgx", databaseURI)
+	if err != nil {
+		return nil, fmt.Errorf("[postgres] failed to connect to DB: %w", err)
+	}
+
+	db.SetMaxOpenConns(30)
+	db.SetMaxIdleConns(30)
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("[postgres] ping failed: %w ", err)
+	}
+
+	log.Println("[postgres] successfull connect to DB")
+	return &Postgres{
+		DB: db,
+	}, nil
+}
+
+func (p *Postgres) Close() error {
+	err := p.DB.Close()
+	if err != nil {
+		log.Printf("[postgres] failed close DB connection: %v", err)
+		return err
+	}
+	log.Println("[postgres] connection closed")
+	return nil
 }
